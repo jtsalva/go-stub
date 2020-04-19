5897d4a5bef1bd71f71f7488b7828c474ba58799
Tim Salva <tim@jtsalva.dev>
2020-04-19 10:10:24 +0100

    :sparkles: Add message when applying blanket CORS

--- middleware.go
+++ middleware.go
@@ -6,7 +6,6 @@ func CORSBlanketMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", config.CorsAllowOrigin)
 		w.Header().Set("Access-Control-Expose-Headers", "*")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
 	})
 }
