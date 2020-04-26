afafdabbb4996988f6ebbae665f6b9d53e6a2a0e
Tim Salva <tim@jtsalva.dev>
2020-04-26 13:09:24 +0100

    :ambulance: Change target name and location of builds

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ do
   os="${parts[0]}"
   arch="${parts[1]}"
   echo "Building $os/$arch"
-  GOOS=$os GOARCH=$arch go build -o "bin/$os/$arch/go-stub" config.go middleware.go stub.go yaml_structs.go main.go
+  GOOS=$os GOARCH=$arch go build -o "bin/$os-$arch-go-stub" config.go middleware.go stub.go yaml_structs.go main.go
 done
 
 echo "Finished building into ./bin"
